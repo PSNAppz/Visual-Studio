f482126c284ea7ae608059e688336939413a9ee9
PSNAppZ <psnappz@gmail.com>
2014-10-22 21:59:16 +0530

    Button Fix

--- WindowsApplication5/Form1.vb
+++ WindowsApplication5/Form1.vb
@@ -45,4 +45,19 @@
         Me.Close()
 
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        WebBrowser1.GoBack()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WebBrowser1.GoForward()
+
+    End Sub
 End Class
